7bea7c7ec3110ff49a0821bbd461231db2111eb9
efesabanogluu <efesabanoglu@gmail.com>
2025-06-28 23:36:19 +0300

    sql config update.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     'unix_socket': '/cloudsql/omega-booster-464215-u4:us-central1:vertigo-master',
-    'user': 'root',       #root dışı user ata.
+    'user': 'efesabanoglu',       #root dışı user ata.
     'password': 'Endeavor1998?',  # sakla bunları.
     'database': 'vertigo_db',
     'autocommit': False,
